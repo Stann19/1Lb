5d484a6fb190a0e540ff3f6b8bccf0c9696ab287
Stann19 <90695056+Stann19@users.noreply.github.com>
2021-09-21 13:37:17 +0300

    звезды
    звезды

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import turtle
 turtle.shape('turtle')
 i=1
-n=50
-turtle.left(90)
-def baterfly(i):
-  turtle.circle(n)
-  turtle.circle(-n)
-while i<=10:
-  baterfly(i)
-  n=n+5
-  i=i+1
-
-
-
+n=5
+turtle.right(180)
+def star(n,i):
+    turtle.left(180-180/n)
+    turtle.forward(100)
+while i<=n:
+    star(n, i)
+    i=i+1
