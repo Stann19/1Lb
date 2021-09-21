75c99aedd3b64af5be91fcdf526da2f93e3799c4
Stann19 <90695056+Stann19@users.noreply.github.com>
2021-09-21 13:38:07 +0300

    квадратная спираль
    квадратная спираль

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import turtle
 turtle.shape('turtle')
-i=1
-n=5
-turtle.right(180)
-def star(n,i):
-    turtle.left(180-180/n)
-    turtle.forward(100)
-while i<=n:
-    star(n, i)
-    i=i+1
+a=8
+i=4
+while a<=200:
+  turtle.forward(a)
+  turtle.left(90)
+  turtle.forward(a)
+  turtle.left(90)
+  a=a+i
