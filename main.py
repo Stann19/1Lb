de9b0b19090ff3d88aa83aa38389f7cf2d65172b
Stann19 <90695056+Stann19@users.noreply.github.com>
2021-09-21 13:38:39 +0300

    окружность
    окружность

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import turtle
+a=0
 turtle.shape('turtle')
-a=8
-i=4
-while a<=200:
-  turtle.forward(a)
-  turtle.left(90)
-  turtle.forward(a)
-  turtle.left(90)
-  a=a+i
+while a<=360:
+    turtle.forward(1)
+    turtle.left(1)
+    a=a+1
+
+
