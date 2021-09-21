a73fa3cb8a81086efcfaad9cc0919ae1d8bbb2a4
Stann19 <90695056+Stann19@users.noreply.github.com>
2021-09-21 13:41:40 +0300

    цветок
    цветок

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import turtle
-from math import sin, cos, pi
 turtle.shape('turtle')
-i=0
-while i<=360:
-    x=i/5*cos(i/5)
-    y=i/5*sin(i/5)
-    turtle.goto(x,y)
-    i=i+1
+n=6
+i=1
+def tsvetok(i):
+  while i<=n:
+     turtle.circle(50)
+     turtle.left(360/n)
+     i=i+1
+tsvetok(i)
