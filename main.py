40a7b7a698c3ae5830e380be0fd2bed065a2b724
Stann19 <90695056+Stann19@users.noreply.github.com>
2021-09-21 13:15:29 +0300

    бабочка
    бабочка

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import turtle
-a=20
-i=1
 turtle.shape('turtle')
+i=1
+n=50
+turtle.left(90)
+def baterfly(i):
+  turtle.circle(n)
+  turtle.circle(-n)
 while i<=10:
-    turtle.forward(a)
-    turtle.left(90)
-    turtle.forward(a)
-    turtle.left(90)
-    turtle.forward(a)
-    turtle.left(90)
-    turtle.forward(a)
-    turtle.right(45)
-    turtle.penup()
-    turtle.forward(14)
-    turtle.left(135)
-    turtle.pendown()
-    a=a+20
-    i=i+1
+  baterfly(i)
+  n=n+5
+  i=i+1
+
+
 
