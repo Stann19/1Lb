e63b4118f9515fe73037c3f3b7195708fbb959f7
Pontis Stas <pstanislav2003@gmail.com>
2021-12-01 21:54:03 +0300

    update

--- поймай шарик 1 и 2/поймай шарик/main.py
+++ поймай шарик 1 и 2/поймай шарик/main.py
@@ -1,5 +1,4 @@
 import pygame
-import time
 from pygame.draw import *
 from random import randint
 
@@ -8,8 +7,7 @@ from random import randint
 В начале открывается окно игры. На игровом поле создается 3 шарика с рандомными цветами 
 и в рандомных местах окна. Аналогично создается 3 кубика. За поподание в шарик- 1 очко, 
 за кубик- 2 очка. Если кубик отразился 4 раза и по нему не попали, то на 5 отражение 
-он исчезнет. После попадания будет повышаться вероятность увеличения сорости шарика и кубика.
-Игра остановится, если нажать на любую клавишу. Счет игрока с его именем заносятся в файл top.txt.
+он исчезнет. Счет игрока с его именем заносятся в файл top.txt.
 '''
 #Считывание имени игрока
 print("Введите ваше имя: ")
@@ -17,7 +15,7 @@ name = str(input())
 
 pygame.init()
 
-FPS = 30
+FPS = 50
 screen = pygame.display.set_mode((1200, 700))
 
 RED = (255, 0, 0)
@@ -28,127 +26,136 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-j = 0
-k = 0
+j = 1
+i = 1
 
-balls_s = []
-balls_r = []
-b_colours = []
-balls_ds = []
-
-class Ball():
-  def __init__(self, x, y, r, color):
+class Ball:
+   def __init__(self, x, y, r, color):
       self.x = x
       self.y = y
       self.r = r
       self.color = color
-  def new_ball(self):
+      self.vx = randint(-5, 5)
+      self.vy = randint(-5, 5)
+   def new_ball(self, screen):
       '''
       функция создает новый шарик с рандомными координатами и цветом
       '''
-      global x, y, r, color, j, k
-      self.x = randint(100, 1100)
-      self.y = randint(100, 600)
-      self.r = randint(30, 50)
-      self.color = COLORS[randint(0, 5)]
-      circle(screen, color, (x, y), r)
-      balls_s.append([x, y])
-      balls_r.append(r)
-      b_colours.append(color)
-      balls_ds.append([randint(-5, 5), randint(-5, 5)])
-  balls = []
-  balls_s = []
-  balls_r = []
-  b_colours = []
-  balls_ds = []
+      circle(screen, self.color, (self.x, self.y), self.r)
 
-  def click_ball(event):
+   def click_ball(self, event):
       '''
       функция проверяет попали ли мы по шару
-      j-отвечает за количество шаров и кубов(суммарно 2*j)
+      j-отвечает за количество шаров(суммарно 2*j)
       score-отвечает за количетво суммарных очков
       '''
-      global j, k, score
-      for i in range(j):
-          if (balls_s[i][0] - event.pos[0]) ** 2 + (balls_s[i][1] - event.pos[1]) ** 2 <= (balls_r[i]) ** 2:
-              score += 1
-              balls_s[i] = [randint(100, 1100), randint(100, 600)]
-              balls_r[i] = randint(20, 50)
-              balls_ds[i] = [randint(-5, 5), randint(-5, 5)]
-              b_colours[i] = COLORS[randint(0, 5)]
+      global score
+      b = event.pos[0]
+      c = event.pos[1]
+      if (self.x - b) ** 2 + (self.y - c) ** 2 <= (self.r) ** 2:
+         score += 1
+         self.x = randint(100, 1100)
+         self.y = randint(100, 600)
+         self.r = randint(20, 50)
+         self.vx = randint(-5, 5)
+         self.vy = randint(-5, 5)
+         self.color = COLORS[randint(0, 5)]
 
-  def move_ball(screen):
-      '''
-      функция осуществляет перемещение и отражение от стенок шаров
-      '''
-      for i in range(j):
-          if balls_s[i][0] + balls_r[i] >= 1200 or balls_s[i][0] - balls_r[i] <= 0:
-              balls_ds[i][0] = -balls_ds[i][0]
-          if balls_s[i][1] + balls_r[i] >= 700 or balls_s[i][1] - balls_r[i] <= 0:
-              balls_ds[i][1] = -balls_ds[i][1]
-          balls_s[i][0] += (1 + score / 7) * balls_ds[i][0]
-          balls_s[i][1] += (1 + score / 7) * balls_ds[i][1]
-          circle(screen, b_colours[i], (balls_s[i][0], balls_s[i][1]), balls_r[i])
+   def move_ball(self):
+       '''
+       функция осуществляет перемещение и отражение от стенок шаров
+       '''
+       global score
+       self.x += 1.5*self.vx
+       self.y += 1.5*self.vy
+       if self.x + self.r >= 1200 or self.x - self.r <= 0:
+           self.vx = -self.vx
+           self.x += self.vx
+           self.y += self.vy
+       if self.y + self.r >= 700 or self.y - self.r <= 0:
+           self.vy = -self.vy
+           self.x += self.vx
+           self.y += self.vy
+       ball1.new_ball(screen)
+       ball2.new_ball(screen)
+       ball3.new_ball(screen)
 
-cubes_s = []
-cubes_a = []
-c_colours = []
-cubes_ds = []
-l=[]
 
-class Cube():
-    def __init__(self):
-    def new_cube():
-      '''
-      функция создает новый кубик с рандомными координатами и цветом
-      '''
-      global x, y, a, color, j, l, k
-      x = randint(100, 1100)
-      y = randint(100, 600)
-      a = randint(40, 60)
-      color = COLORS[randint(0, 5)]
-      rect(screen, color, (x, y, a, a))
-      cubes_s.append([x, y])
-      cubes_a.append(a)
-      c_colours.append(color)
-      cubes_ds.append([randint(-5, 5), randint(-5, 5)])
-      l.append(0)
+class Cube:
+    def __init__(self, x, y, a, color):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.vx = randint(-5, 5)
+        self.vy = randint(-5, 5)
+        self.color = color
+        self.l = 0
+    def new_cube(self, screen):
+       '''
+       функция создает новый кубик с рандомными координатами и цветом
+       '''
+       rect(screen, self.color, (self.x, self.y, self.a, self.a))
 
-    cubes_s = []
-    cubes_a = []
-    c_colours = []
-    cubes_ds = []
-    l = []
+    def click_cube(self, event):
+        '''
+        функция проверяет попали ли мы по кубу
+        2*j-отвечает за количество кубов
+        score-отвечает за количетво суммарных очков
+        '''
+        global score
+        b = event.pos[0]
+        c = event.pos[1]
+        if (self.x - b) ** 2 + (self.y - c) ** 2 <= (self.a) ** 2:
+            score += 2
+            self.x = randint(100, 1100)
+            self.y = randint(100, 600)
+            self.a = randint(30, 60)
+            self.vx = randint(-5, 5)
+            self.vy = randint(-5, 5)
+            self.l = 0
+            self.color = COLORS[randint(0, 5)]
 
-    def move_cube(screen):
+    def move_cube(self):
         '''
         функция осуществляет перемещение и отражение от стенок кубов,
         а также ставит условие на количество отражений кубика
         '''
-        for i in range(k):
-            if cubes_s[i][0] + cubes_a[i] >= 1200 or cubes_s[i][0] <= 0:
-                cubes_ds[i][0] = -cubes_ds[i][0]
-                l[i] += 1
-            if cubes_s[i][1] + cubes_a[i] >= 700 or cubes_s[i][1] <= 0:
-                cubes_ds[i][1] = -cubes_ds[i][1]
-                l[i] += 1
-            # Если соударений произошло больше, чем четрыре, то кубик "удаляется"
-            if l[i] > 4:
-                c_colours[i] = (0, 0, 0)
-                cubes_ds[i][0] = 0
-                cubes_ds[i][1] = 0
-                cubes_s[i][0] = 1400
-                cubes_s[i][1] = 1200
-            cubes_s[i][0] += (1 + score / 10) * cubes_ds[i][0]
-            cubes_s[i][1] += (1 + score / 10) * cubes_ds[i][1]
-            rect(screen, c_colours[i], (cubes_s[i][0], cubes_s[i][1], cubes_a[i], cubes_a[i]))
+        self.x += 1.5*self.vx
+        self.y += 1.5*self.vy
+        if self.x + self.a >= 1200 or self.x <= 0:
+            self.vx = -self.vx
+            self.l += 1
+            self.x += self.vx
+            self.y += self.vy
+        if self.y + self.a >= 700 or self.y <= 0:
+            self.vy = -self.vy
+            self.l += 1
+            self.x += self.vx
+            self.y += self.vy
+        # Если соударений произошло больше, чем четрыре, то кубик "удаляется"
+        if self.l > 4:
+            self.color = (0, 0, 0)
+            self.vx = 0
+            self.vy = 0
+            self.x = 1400
+            self.y = 1200
+        cube1.new_cube(screen)
+        cube2.new_cube(screen)
+        cube3.new_cube(screen)
 
-for i in range(3):
-    ball = Ball(randint(30, 50), COLORS[randint(0, 5)], randint(100, 1100), randint(100, 600), randint(-5, 5), randint(-5, 5), 0)
-    balls.append(ball)
-for i in range(3):
-    cube = Cube(COLORS[randint(0, 5)], randint(100, 1100), randint(100, 600), randint(40, 60), randint(-5, 5), randint(-5, 5), 0, 0)
-    cubes.append(cube)
+score = 0
+ball1 = Ball(randint(100, 1100), randint(100, 600), randint(30, 50), COLORS[randint(0, 5)])
+cube1 = Cube(randint(100, 1100), randint(100, 600), randint(40, 60), COLORS[randint(0, 5)])
+ball2 = Ball(randint(100, 1100), randint(100, 600), randint(30, 50), COLORS[randint(0, 5)])
+cube2 = Cube(randint(100, 1100), randint(100, 600), randint(40, 60), COLORS[randint(0, 5)])
+ball3 = Ball(randint(100, 1100), randint(100, 600), randint(30, 50), COLORS[randint(0, 5)])
+cube3 = Cube(randint(100, 1100), randint(100, 600), randint(40, 60), COLORS[randint(0, 5)])
+ball1.new_ball(screen)
+cube1.new_cube(screen)
+ball2.new_ball(screen)
+cube2.new_cube(screen)
+ball3.new_ball(screen)
+cube3.new_cube(screen)
 
 FONT = pygame.font.Font(None, 50)
 
@@ -156,13 +163,6 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-score = 0
-for i in range(3):
-    new_ball()
-    j += 1
-for i in range(3):
-    new_cube()
-    k += 1
 
 FONT = pygame.font.Font(None, 50)
 
@@ -172,9 +172,19 @@ while not finished:
         if event.type == pygame.KEYDOWN:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click(event)
+            cube1.click_cube(event)
+            cube2.click_cube(event)
+            cube3.click_cube(event)
+            ball1.click_ball(event)
+            ball2.click_ball(event)
+            ball3.click_ball(event)
 
-    move(screen)
+    cube1.move_cube()
+    cube2.move_cube()
+    cube3.move_cube()
+    ball1.move_ball()
+    ball2.move_ball()
+    ball3.move_ball()
 
     score_display = FONT.render(str(score), True, (255, 255, 255))
     screen.blit(score_display, (10, 10))
